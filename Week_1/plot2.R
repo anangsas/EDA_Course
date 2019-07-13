c72c4f1dda2fa79ee2bf472951f49708d8f55c1d
anangsas <anangsa@gmail.com>
2019-07-13 20:09:01 +0700

    Add plot2 update plot1

--- Week_1/plot2.R
+++ Week_1/plot2.R
@@ -2,6 +2,9 @@ power_con <- read.table("household_power_consumption.txt",skip=1,sep=";")
 names(power_con) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 sub1 <- subset(power_con,power_con$Date=="1/2/2007" | power_con$Date =="2/2/2007")
 
-png("plot1.png", width=480, height=480)
-hist(as.numeric(as.character(sub1$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+sub1$DateTime <- with(sub1, strptime(paste(Date, Time, sep=" "), "%d/%m/%Y %H:%M:%S"))
+
+
+png("plot2.png", width=480, height=480)
+plot(sub1$DateTime,as.numeric(as.character(sub1$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
 dev.off()
